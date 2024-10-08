ac7dd43c1fdc1dd949e7901277fc1e1ca035cea5
mdsaiful170 <saifulsylhet00@gmail.com>
2024-10-08 12:41:42 +0600

    all-update

--- src/componets/Location.jsx
+++ src/componets/Location.jsx
@@ -3,7 +3,7 @@ const Location = () => {
     <>
       <section style={{marginTop:"90px"}}>
         <div className="position-relative" >
-          <div className="position-absolute top-0 " style={{textAlign:'center', left:'40%'} }>
+          <div className="position-absolute top-0 w-100 text-center ">
             <h1 style={{color:'#0D0A19' , fontWeight:'700', fontSize:'1.7rem'}}>Find vehicles near you</h1>
           </div>
           <img src="Img/Map.png" />
